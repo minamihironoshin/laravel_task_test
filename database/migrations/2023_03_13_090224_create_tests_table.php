de090379e12c90858eb0628ffeb1e8fcf9bf4ebb
南博之伸 <minamihironoshin@minamihiroyukishinnoMacBook-Pro.local>
2023-03-27 09:59:37 +0900

    first

---
+++ database/migrations/2023_03_13_090224_create_tests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->timestamps(); //create at & update at が追加される
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+};
